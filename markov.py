112f095dbd85550da80d15aa1f357910e6985bc1
eric <eric_rosen@brown.edu>
2022-04-11 14:50:31 -0400

    Nick's changes, focuses on food now

--- markov.py
+++ markov.py
@@ -11,7 +11,7 @@ def read_csv(path):
 			full_line = line.strip()
 			split_line = full_line.split(",")
 			split_line = split_line[1:]
-			total_line = ""
+			total_line = " "
 			for entry in split_line:
 				total_line += entry
 			if cnt != 1:
@@ -20,10 +20,11 @@ def read_csv(path):
 			cnt += 1
 	return(all_lines)
 
+
 def prepare_markov(data):
-	markov_dict = {} 
+	markov_dict = {}
 	split_spaces = data.split(" ")
-	
+
 	for word1 in range(len(split_spaces)-1):
 		if split_spaces[word1] not in markov_dict.keys():
 			markov_dict[split_spaces[word1]] = {split_spaces[word1+1]:1}
@@ -32,8 +33,8 @@ def prepare_markov(data):
 				markov_dict[split_spaces[word1]][split_spaces[word1+1]] = 1
 			else:
 				markov_dict[split_spaces[word1]][split_spaces[word1+1]] += 1
-	#print string_phrase
 	return(markov_dict)
+
 
 def next_essay_word(wword,markov_dict):
 	word2_dict = markov_dict[wword]
@@ -44,13 +45,12 @@ def next_essay_word(wword,markov_dict):
 	draw = choice(word2_keys, 1,
               p=word2_probs)
 	return(draw[0])
-	#nword = random.choice(markov_dict[wword].keys())
-	#return nword
+
 
 def produce_sentences(markov,starting_word):
 	cur_word = starting_word
 	full_string = cur_word
-	for _ in range(10):
+	for _ in range(50):
 		try:
 			cur_word = next_essay_word(cur_word,markov_dict)
 			full_string += " " + cur_word
@@ -61,5 +61,5 @@ def produce_sentences(markov,starting_word):
 path = "./form.csv"
 data = read_csv(path)
 markov_dict = prepare_markov(data)
-essay = produce_sentences(markov_dict,"have")
-print(essay)
+essay = produce_sentences(markov_dict,"is")
+print("My favorite food " + essay)
